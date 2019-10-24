c719c1d9b10fb8e2feda1edc32f762dfb4ccdb8a
Clifford Tran <clipqq>
2019-10-24 09:26:46 -0700

    refactored routers into single file

--- src/app.js
+++ src/app.js
@@ -10,10 +10,7 @@ const uuid = require('uuid/v4');
 const bodyParser = require('body-parser')
 const logger = require('./logger')
 
-const bookmarkGET = require('./bookmark/bookmark-get')
-const bookmarkGETid = require('./bookmark/bookmark-get-id')
-const bookmarkPOST = require('./bookmark/bookmark-post')
-const bookmarkDELETE = require('./bookmark/bookmark-delete')
+const bookmarkRouters = require('./bookmark/bookmark-routers')
 
 const app = express()
 
@@ -27,10 +24,7 @@ app.use(helmet())
 app.use(express.json());
 app.use(bodyParser())
 
-app.use(bookmarkGET)
-app.use(bookmarkGETid)
-app.use(bookmarkPOST)
-app.use(bookmarkDELETE)
+app.use(bookmarkRouters)
 
 /////////////////////////
 
@@ -45,213 +39,7 @@ app.use(function validateBearerToken(req, res, next) {
   next()
 })
 
-const cards = [{
-  id: 1,
-  title: 'Task One',
-  content: 'This is card one'
-}];
 
-const lists = [{
-  id: 1,
-  header: 'List One',
-  cardIds: [1]
-}];
-
-const BOOKMARK = require('./bookmark/bookmark') // new data store
-
-app.get('/', (req, res) => {
-  res
-    .send('It works!');
-});
-
-app.get('/card', (req, res) => {
-  res
-    .json(cards);
-});
-
-app.get('/card/:id', (req, res) => {
-  const {
-    id
-  } = req.params;
-  const card = cards.find(c => c.id == id);
-
-  // make sure we found a card
-  if (!card) {
-    logger.error(`Card with id ${id} not found.`);
-    return res
-      .status(404)
-      .send('Card Not Found');
-  }
-
-  res.json(card);
-});
-
-app.post('/card', (req, res) => {
-  const {
-    title,
-    content
-  } = req.body;
-
-  if (!title) {
-    logger.error(`Title is required`);
-    return res
-      .status(400)
-      .send('Invalid data');
-  }
-
-  if (!content) {
-    logger.error(`Content is required`);
-    return res
-      .status(400)
-      .send('Invalid data');
-  }
-
-  const id = uuid();
-
-  const card = {
-    id,
-    title,
-    content
-  };
-
-  cards.push(card);
-
-  logger.info(`Card with id ${id} created`);
-
-  res
-    .status(201)
-    .location(`http://localhost:8000/card/${id}`)
-    .json({
-      id
-    });
-})
-
-app.delete('/card/:id', (req, res) => {
-  const {
-    id
-  } = req.params;
-
-  const cardIndex = cards.findIndex(c => c.id === id);
-
-  if (cardIndex === -1) {
-    logger.error(`Card with id ${id} not found.`);
-    return res
-      .status(404)
-      .send('Not found');
-  }
-
-  //remove card from lists
-  //assume cardIds are not duplicated in the cardIds array
-  lists.forEach(list => {
-    const cardIds = list.cardIds.filter(cid => cid !== id);
-    list.cardIds = cardIds;
-  });
-
-  cards.splice(cardIndex, 1);
-
-  logger.info(`Card with id ${id} deleted.`);
-
-  res
-    .status(204)
-    .end();
-});
-
-app.get('/list', (req, res) => {
-  res
-    .json(lists);
-});
-
-app.get('/list/:id', (req, res) => {
-  const {
-    id
-  } = req.params;
-  const list = lists.find(li => li.id == id);
-
-  // make sure we found a list
-  if (!list) {
-    logger.error(`List with id ${id} not found.`);
-    return res
-      .status(404)
-      .send('List Not Found');
-  }
-
-  res.json(list);
-});
-
-app.post('/list', (req, res) => {
-  const {
-    header,
-    cardIds = []
-  } = req.body;
-  if (!header) {
-    logger.error(`Header is required`);
-    return res
-      .status(400)
-      .send('Invalid data');
-  }
-
-  // check card ids
-  if (cardIds.length > 0) {
-    let valid = true;
-    cardIds.forEach(cid => {
-      const card = cards.find(c => c.id == cid);
-      if (!card) {
-        logger.error(`Card with id ${cid} not found in cards array.`);
-        valid = false;
-      }
-    });
-
-    if (!valid) {
-      return res
-        .status(400)
-        .send('Invalid data');
-    }
-  }
-
-  // get an id
-  const id = uuid();
-
-  const list = {
-    id,
-    header,
-    cardIds
-  };
-
-  lists.push(list);
-
-  logger.info(`List with id ${id} created`);
-
-  res
-    .status(201)
-    .location(`http://localhost:8000/list/${id}`)
-    .json({
-      id
-    });
-
-});
-
-app.delete('/list/:id', (req, res) => {
-  const {
-    id
-  } = req.params;
-
-  const listIndex = lists.findIndex(li => li.id === id);
-
-  if (listIndex === -1) {
-    logger.error(`List with id ${id} not found.`);
-    return res
-      .status(404)
-      .send('Not Found');
-  }
-
-  lists.splice(listIndex, 1);
-
-  logger.info(`List with id ${id} deleted.`);
-  res
-    .status(204)
-    .end();
-
-});
 
 ///////////////////////
 
---
+++ src/bookmark/bookmark-routers.js
@@ -0,0 +1,120 @@
+const express = require('express')
+const bookmarkRouters = express.Router()
+const bodyParser = express.json()
+const BOOKMARK = require('./bookmark') // new data store
+const logger = require('../logger')
+const uuidv4 = require('uuid/v4')
+
+
+bookmarkRouters.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`);
+        return res.status(401).json({
+            error: 'Unauthorized request'
+        })
+    }
+    // move to the next middleware
+    next()
+})
+
+// homepage
+bookmarkRouters.get('/', (req, res) => {
+    res
+        .send('Homepage works!');
+});
+
+// get all bookmarks
+bookmarkRouters.route('/bookmark').get((req, res) => {
+    res
+        .json(BOOKMARK);
+})
+
+// get by id
+bookmarkRouters.get('/bookmark/:id', (req, res) => {
+    const {
+        id
+    } = req.params;
+    const bookmark = BOOKMARK.find(c => c.id == id);
+
+    // make sure we found a bookmark
+    if (!bookmark) {
+        logger.error(`Bookmark with id ${id} not found.`);
+        return res
+            .status(404)
+            .send('Bookmark Not Found');
+    }
+
+    res.json(bookmark);
+});
+
+// post new bookmark
+bookmarkRouters.route('/bookmark')
+    .post((req, res) => {
+        console.log(`req.body`, req.body)
+
+        const {
+            title,
+            content
+        } = req.body;
+        if (!title) {
+            logger.error(`Title is required`);
+            return res
+                .status(400)
+                .send('Invalid data');
+        }
+
+        if (!content) {
+            logger.error(`Content is required`);
+            return res
+                .status(400)
+                .send('Invalid data');
+        }
+
+        console.log(`before uuid`)
+        const id = uuidv4();
+        console.log(`passed uuid`)
+
+        const bookmark = {
+            id,
+            title,
+            content
+        };
+        BOOKMARK.push(bookmark);
+
+        logger.info(`Bookmark with id ${id} created`);
+
+        res
+            .status(201)
+            .location(`http://localhost:8000/bookmark/${id}`)
+            .json({
+                id
+            });
+    })
+
+// delete bookmark by id
+bookmarkRouters.delete('/bookmark/:id', (req, res) => {
+    const {
+        id
+    } = req.params;
+    const bookmarkIndex = BOOKMARK.findIndex(c => c.id.toString() === id);
+
+    if (bookmarkIndex === -1) {
+        logger.error(`bookmark with id ${id} not found.`);
+        return res
+            .status(404)
+            .send('Not found');
+    }
+
+    BOOKMARK.splice(bookmarkIndex, 1);
+
+    logger.info(`bookmark with id ${id} deleted.`);
+
+    res
+        .status(204)
+        .end();
+});
+
+
+module.exports = bookmarkRouters
